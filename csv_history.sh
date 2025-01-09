c0db18a6901884b40cc84760816eadd64f3db5d7
Simon Schwitz <simon@calucon.de>
2025-01-09 18:31:03 +0100

    improve history script

--- csv_history.sh
+++ csv_history.sh
@@ -3,9 +3,10 @@
 LAST_MODIFIED_FILE=".last_modified"
 CSV_FILE="players.csv"
 DATE_FORMAT="+%Y-%d-%mT%H:%M:%S"
+ARCHIVE_DIR="archive/"
 
-mkdir -p archive/
 touch $LAST_MODIFIED_FILE
+mkdir -p $ARCHIVE_DIR
 
 if [ ! -f $CSV_FILE ]; then
     exit 1
@@ -15,13 +16,14 @@ LAST_MODIFIED=$(cat "$LAST_MODIFIED_FILE")
 MODIFIED_DATE=$(date -r "$CSV_FILE" "$DATE_FORMAT")
 
 # write last modified to file if not exists yet
-if [ -z "$LAST_MODIFIED" ]; then
-    LAST_MODIFIED="$MODIFIED_DATE"
-    echo "$LAST_MODIFIED" >$LAST_MODIFIED_FILE
-fi
+#if [ -z "$LAST_MODIFIED" ]; then
+#    LAST_MODIFIED="$MODIFIED_DATE"
+#    echo "$LAST_MODIFIED" >$LAST_MODIFIED_FILE
+#fi
 
 # archive current file version
 if [ "$LAST_MODIFIED" != "$MODIFIED_DATE" ]; then
     cp "$CSV_FILE" "archive/players_${MODIFIED_DATE}.csv"
     echo "$LAST_MODIFIED" >$LAST_MODIFIED_FILE
+    echo "Modification detected, creating Archive!"
 fi
